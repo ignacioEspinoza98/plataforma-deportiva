2bb45e99e3a205ff0f721372bb84e85a98688dc7
Ignacio Espinoza <ignacioespinozaruz@gmail.com>
2025-01-15 14:08:00 -0300

    Add files via upload

---
+++ plataforma_deportiva/plataforma_deportiva_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PlataformaDeportivaAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'plataforma_deportiva_app'
